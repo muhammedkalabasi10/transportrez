1058fe137acaf4e6a18f088ac5da4eed4071c7db
Muhammed Kalabaşı <97872051+muhammedkalabasi10@users.noreply.github.com>
2024-02-18 20:25:04 +0300

    first commit

---
+++ src/main/java/tp/rez/RezApplication.java
@@ -0,0 +1,13 @@
+package tp.rez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezApplication.class, args);
+	}
+
+}
